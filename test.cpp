894cb6730a339a21ce5cf5d0cf3f41b400175978
Andrew Katson <katsonandrew3.5@gmail.com>
2024-03-01 13:39:43 -0500

    Fix the main file a bit

--- test.cpp
+++ test.cpp
@@ -1,9 +1,9 @@
-#include "test_proto.pb.h"
+#include "test.pb.h"
 #include "boost/chrono.hpp"
 
 int main(int argc, char const * argv[]) {
-	
+
   protobuf_not_included::TestProto testProto;
-  boost::chrono::millseconds ms(100);
+  boost::chrono::milliseconds ms(100);
   return 0;
 }
